0393ea4491672b263e7c9b282cfcac284aa38cdf
Adam2he <114520332+Adam2he@users.noreply.github.com>
2024-02-14 20:22:25 +0100

    Update planificador.py

--- planificador.py
+++ planificador.py
@@ -25,28 +25,48 @@ def planificador(posicion,goal,m,F):
         datos+=posicion
     return datos
 
-
-def planificador2(posicion,goal,m,F,N):
-    k=0
-    posicionA=posicion
-    while calcula_distancia(posicion,goal) > m:
+def planificador_pruebas(posicion,goal,m,F):
+    datos = []
+    cont=0
+    posicion_ant=[0,0,0]
+    posicion_1=[0,0,0]
+    while calcula_distancia(posicion, goal) > m and cont<1000:
         try:
             posicion[0] = posicion[0] + F[round(posicion[0])][round(posicion[1])][round(posicion[2])][0]
         except:
-            posicion[0] = posicionA[0]
+            posicion[0]=posicion_ant[0]
         try:
             posicion[1] = posicion[1] + F[round(posicion[0])][round(posicion[1])][round(posicion[2])][1]
         except:
-            posicion[1] = posicionA[1]
+            posicion[1]=posicion_ant[1]
         try:
             posicion[2] = posicion[2] + F[round(posicion[0])][round(posicion[1])][round(posicion[2])][2]
         except:
-            posicion[2] = posicionA[2]
-        posicionA = posicion
-        k+=1
-        if k>300:
-            return False
-    return True
+            posicion[2]=posicion_ant[2]
+        datos += posicion
+
+        posicion_ant[:]=posicion_1[:]
+        posicion_1[:] = posicion[:]
+        cont+=1
+
+    if cont == 1000:
+        return [0, 0, 0], cont
+    else:
+        return datos, cont
+
+def planificador_nuevo(posicion,goal,m,obstacle,m_goal,R_soi):
+    datos=[]
+    while calcula_distancia(posicion,goal) > m:
+        fuerzas=m_goal*np.array([goal[0]-posicion[0], goal[1] - posicion[1], goal[2] - posicion[2]]) / calcula_distancia(posicion,goal)
+        for i in range(len(obstacle)):
+            distancia=calcula_distancia(posicion,obstacle[i])
+            if distancia<R_soi:
+                fuerzas+=np.array([posicion[0]-obstacle[i][0],posicion[1]-obstacle[i][1],posicion[2]-obstacle[i][2]])*((R_soi-distancia)/distancia)/distancia
+        posicion[0] = posicion[0] + fuerzas[0]
+        posicion[1] = posicion[1] + fuerzas[1]
+        posicion[2] = posicion[2] + fuerzas[2]
+        datos+=posicion
+    return datos
 
 def planificador_con_representacion(posicion,goal,m,F,obstacle):
     # Crear una figura 3D
@@ -70,6 +90,102 @@ def planificador_con_representacion(posicion,goal,m,F,obstacle):
         plt.pause(0.01)
     plt.show()
     return datos
+
+
+
+def planificador_nuevo_ajustable(posicion,goal,m,obstacle,m_goal,R_soi,N):
+    datos=[]
+    posicion_ant=[0,0,0]
+    k=1 #Variable para salir del mínimo local
+    cont=0
+    while calcula_distancia(posicion,goal) > m and cont<1000:
+        fuerzas=k*m_goal*np.array([goal[0]-posicion[0], goal[1] - posicion[1], goal[2] - posicion[2]]) / calcula_distancia(posicion,goal)
+        for i in range(len(obstacle)):
+            distancia=calcula_distancia(posicion,obstacle[i])
+            if distancia<R_soi:
+                fuerzas+=np.array([posicion[0]-obstacle[i][0],posicion[1]-obstacle[i][1],posicion[2]-obstacle[i][2]])*((R_soi-distancia)/distancia)/distancia
+        posicion[0] += fuerzas[0]
+        posicion[1] += fuerzas[1]
+        posicion[2] += fuerzas[2]
+        if posicion[0] > N or posicion[0] < 0:
+            posicion[0]=posicion_ant[0]
+        if posicion[1] > N or posicion[1] < 0:
+            posicion[1]=posicion_ant[1]
+        if posicion[2] > N or posicion[2] < 0:
+            posicion[2]=posicion_ant[2]
+        if calcula_distancia(posicion,posicion_ant) < 0.5:
+            k+=0.1
+        else:
+            k=1
+        posicion_ant[:]=posicion[:]
+        datos+=posicion
+        cont+=1
+    if cont==1000:
+        return [0,0,0],cont
+    else:
+        return datos,cont
+
+def planificador_nuevo_ajustable_pruebas(posicion,goal,m,obstacle,m_goal,R_soi,N):
+    datos=[]
+    posicion_ant=[0,0,0]
+    k=1 #Variable para salir del mínimo local
+    cont=0
+    while calcula_distancia(posicion,goal) > m and cont<1000:
+        fuerzas=k*m_goal*np.array([goal[0]-posicion[0], goal[1] - posicion[1], goal[2] - posicion[2]]) / calcula_distancia(posicion,goal)
+        for i in range(len(obstacle)):
+            distancia=calcula_distancia(posicion,obstacle[i])
+            if distancia<R_soi:
+                fuerzas+=np.array([posicion[0]-obstacle[i][0],posicion[1]-obstacle[i][1],posicion[2]-obstacle[i][2]])*((R_soi-distancia)/distancia)/distancia
+        posicion[0] += fuerzas[0]
+        posicion[1] += fuerzas[1]
+        posicion[2] += fuerzas[2]
+        if posicion[0] > N or posicion[0] < 0:
+            posicion[0]=posicion_ant[0]
+        if posicion[1] > N or posicion[1] < 0:
+            posicion[1]=posicion_ant[1]
+        if posicion[2] > N or posicion[2] < 0:
+            posicion[2]=posicion_ant[2]
+        posicion_ant[:]=posicion[:]
+        datos+=posicion
+        cont+=1
+    if cont==1000:
+        return [0,0,0],cont
+    else:
+        return datos,cont
+
+
+def planificador_nuevo_ajustable2(posicion,goal,m,obstacle,m_goal,R_soi,N):
+    datos=[]
+    posicion_ant=[0,0,0]
+    cont=0
+    k=1
+    while calcula_distancia(posicion,goal) > m and cont<1000:
+        fuerzas=k*m_goal*np.array([goal[0]-posicion[0], goal[1] - posicion[1], goal[2] - posicion[2]]) / calcula_distancia(posicion,goal)
+        for i in range(len(obstacle)):
+            distancia=calcula_distancia(posicion,obstacle[i])
+            if distancia<R_soi:
+                fuerzas+=np.array([posicion[0]-obstacle[i][0],posicion[1]-obstacle[i][1],posicion[2]-obstacle[i][2]])*((R_soi-distancia)/distancia)/distancia
+        posicion[0] += fuerzas[0]
+        posicion[1] += fuerzas[1]
+        posicion[2] += fuerzas[2]
+        if posicion[0] > N or posicion[0] < 0:
+            posicion[0]=posicion_ant[0]
+        if posicion[1] > N or posicion[1] < 0:
+            posicion[1]=posicion_ant[1]
+        if posicion[2] > N or posicion[2] < 0:
+            posicion[2]=posicion_ant[2]
+        if calcula_distancia(posicion,posicion_ant) < 0.5:
+            R_soi-=0.1
+        else:
+            R_soi=5
+        posicion_ant[:]=posicion[:]
+        datos+=posicion
+        cont+=1
+    if cont==1000:
+        return [0,0,0],cont
+    else:
+        return datos,cont
+
 
 
 
